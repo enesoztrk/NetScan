8c63168c67dd223719405c5a97ab93f7f141d271
Enes <enes.ozturk645@gmail.com>
2021-10-27 00:44:07 +0300

    state machine source files have been added

---
+++ NetScanApp/inc/ns_SM_common.h
@@ -0,0 +1,54 @@
+#ifndef NS_SM_COMMON_H
+#define NS_SM_COMMON_H
+#include <tinyfsm.hpp>
+#include <iostream>
+#include <stdlib.h> /* rand */
+#include <map>
+
+namespace NetScan_SM {
+    using namespace tinyfsm;
+    uint32_t get_ticks_passed_until_now(void);
+    using cb_t = bool(*)(int, void*);
+    enum class States {
+        ARP_MSG_SEND = 0,
+        DNS_MSG_SEND,
+        ARP_MSG_PARSE,
+        DNS_MSG_PARSE,
+        INACTIVE,
+        COMM_TIMEOUT
+    };
+
+    //State lookup table
+    std::map<States, std::string> States_table{
+
+    {States::ARP_MSG_SEND,"ARP-MSG-SENT"},
+    {States::DNS_MSG_SEND,"DNS-MSG-SENT"},
+    {States::ARP_MSG_PARSE,"ARP-MSG-PARSE"},
+    {States::DNS_MSG_PARSE,"DNS-MSG-PARSE"},
+    {States::INACTIVE,"INACTIVE"},
+    {States::COMM_TIMEOUT,"COMM-TIMEOUT"},
+
+
+    };
+
+    template<typename... FF>
+    struct ST_NetScan_SM_List;
+
+    template<> struct ST_NetScan_SM_List<> : tinyfsm::FsmList<> {
+
+        static void register_callback(const cb_t& cb, States cb_state) {}
+
+    };
+
+    template<typename F, typename... FF>
+    struct ST_NetScan_SM_List<F, FF...> : tinyfsm::FsmList<F, FF...>
+    {
+
+        static void register_callback(const cb_t& cb, States cb_state) {
+            F::register_callback(cb, cb_state);
+            ST_NetScan_SM_List<FF...>::register_callback(cb,cb_state);
+        }
+
+    };
+}
+#endif // NS_SM_COMMON_H
